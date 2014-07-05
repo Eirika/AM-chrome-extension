84bb2f03bb2dce9129be063a0c5abc42c50bfca5
Amoki <hugoduroux@gmail.com>
2014-07-05 13:19:41 +0200

    post musique.amoki.fr

--- send.js
+++ send.js
@@ -2,7 +2,7 @@ chrome.tabs.getSelected(null, function(tab) {
   if(tab.url.indexOf("https://youtube.com" !== -1)) {
     jQuery.ajax({
       type: "POST",
-      url: "http://127.0.0.1:8000",
+      url: "http://musique.amoki.fr",
       data: {
         'url': tab.url,
         'add_url': true
